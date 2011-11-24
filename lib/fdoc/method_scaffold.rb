651cf51c5d6fe2362edf03fa96741eb276b3a848
Zach Margolis <margolis@squareup.com>
2011-11-24 13:39:39 -0800

    Scaffold cleanup

--- lib/fdoc/method_scaffold.rb
+++ lib/fdoc/method_scaffold.rb
@@ -1,8 +1,34 @@
 class Fdoc::MethodScaffold
   def initialize(method_name)
-    @scaffolded_method = Fdoc::Method.new(:partial_data => {"Name" => method_name.to_s})
+    @scaffolded_method = Fdoc::Method.new(:partial_data => {})
+    @scaffolded_method.name = method_name.to_s
+    @scaffolded_method.verb = "???"
+    @scaffolded_method.description = "???"
   end
 
+  def scaffold_request(params)
+    params.map do |key, value|
+      unless @scaffolded_method.request_parameter_named(key)
+        @scaffolded_method.request_parameters << scaffold_param(key, value, Fdoc::RequestParameter)
+      end
+    end
+  end
+
+  def scaffold_response(params, rails_response, successful)
+    params.map do |key, value|
+      unless @scaffolded_method.response_parameter_named(key)
+        @scaffolded_method.response_parameters << scaffold_param(key, value, Fdoc::ResponseParameter)
+      end
+    end
+
+    unless @scaffolded_method.response_code_for(rails_response, successful)
+      rc = Fdoc::ResponseCode.new(:partial_data => {})
+      rc.response_code = rails_response
+      rc.succesful = successful
+      @scaffolded_method.response_codes << rc
+    end
+  end
+  
   def scaffold_param(name, value, param_class)
     param = param_class.new(:partial_data => {})
     param.name = name
@@ -14,28 +40,5 @@ class Fdoc::MethodScaffold
       param.required = "???"
     end
     param
-  end
-
-  def scaffold_request(params)
-    params.map do |key, value|
-      unless @scaffolded_method.request_parameter_named(key)
-        @scaffolded_method.request_parameters << scaffold_parameter(key, value, Fdoc::RequestParameter)
-      end
-    end
-  end
-
-  def scaffold_response(params, rails_response, successful)
-    params.map do |key, value|
-      unless @scaffolded_method.response_parameter_named(key)
-        @scaffolded_method.response_parameters << scaffold_parameter(key, value, Fdoc::ResponseParameter)
-      end
-    end
-
-    unless @scaffolded_method.response_code_for(rails_response, successful)
-      rc = Fdoc::ResponseCode.new(:partial_data => {})
-      rc.response_code = rails_response
-      rc.succesful = successful
-      @scaffolded_method.response_codes << rc
-    end
   end
 end
--- lib/fdoc/resource_scaffold.rb
+++ lib/fdoc/resource_scaffold.rb
@@ -4,8 +4,9 @@ class Fdoc::ResourceScaffold
     camel_case_resource = controller.split(':').last.match(/(.*)(?:Controller?)/)[1]
     snake_case_resource = camel_case_resource.gsub(/^([A-Z])/) {|m| m.downcase}.gsub(/([A-Z])/) {|m| "_#{m.downcase}" }
     @scaffolded_resource.name = snake_case_resource
-    @scaffolded_resource.verb = "???"
-    @scaffolded_resource.action = "???"
+    @scaffolded_resource.controller = controller_name
+    @scaffolded_resource.description = "???"
+    @scaffolded_resource.base_path = "???"
   end
 
   alias create_or_load initialize
