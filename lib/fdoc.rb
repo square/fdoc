aa611fbde2adfc231f402ce14f57bc014bc79e1c
Zach Margolis <margolis@squareup.com>
2011-11-27 12:28:15 -0800

    Rip page classes into separate file

--- lib/fdoc.rb
+++ lib/fdoc.rb
@@ -2,50 +2,6 @@ lib_dir = File.expand_path(File.dirname(__FILE__) + "/fdoc")
 $:.unshift(lib_dir)
 
 module Fdoc
-  class Page
-    def get_binding
-      binding
-    end
-
-    def index_path
-      path = @base_path
-      path = "/" + path unless @options[:html]
-      path
-    end
-
-    def css_path
-      path = "#{@base_path}/main.css"
-      path = "/" + path unless @options[:html]
-      path
-    end
-  end
-  
-  class DirectoryPage < Page
-    def initialize(resources, base_path, options = {})
-      @resources = resources
-      @base_path = base_path
-      @options = options
-    end
-    
-    def resource_path(resource)
-      path = "#{@base_path}/#{resource.name}"
-      if @options[:html]
-        path += ".html"
-      else
-        path = "/" + path
-      end
-      path
-    end    
-  end  
-
-  class ResourcePage < Page
-    def initialize(resource, base_path, options = {})
-      @resource = resource
-      @base_path = base_path
-      @options = options
-    end
-  end
-
   def self.load(path = 'docs/fdoc')
     @resource_checklists = {}
 
@@ -110,3 +66,4 @@ require 'parameter'
 require 'response_code'
 require 'request_parameter'
 require 'response_parameter'
+require 'page'
