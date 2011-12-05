a22119145bae384b932da0d314b734da181d22ea
Sean Sorrell <github+ss@git.squareup.com>
2011-12-05 10:34:38 -0800

    wrap long comments

--- lib/fdoc/doc_node.rb
+++ lib/fdoc/doc_node.rb
@@ -1,6 +1,6 @@
 =begin
 
-Fdoc::Node is the abstract, meaning-free mapping tool. Within the realm of documentation, Fdoc::DocNode is the  meaningful parent class.
+Fdoc::Node is the abstract, meaning-free mapping tool. Within the realm of documentation, Fdoc::DocNode is the meaningful parent class.
 
 Every node in the fdoc graph can take a description or be marked as deprecated.
 
