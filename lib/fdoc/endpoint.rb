e493f6b3c98f1f09600d78ad13c4c82e373c4db2
Jack Danger Canty <jackdanger@squareup.com>
2013-03-27 12:04:01 -0700

    blech. whitespace broke.

--- lib/fdoc/endpoint.rb
+++ lib/fdoc/endpoint.rb
@@ -23,8 +23,8 @@ class Fdoc::Endpoint
   def consume_response(params, status_code, successful=true)
     response_code = response_codes.find do |rc|
       rc["successful"] == successful && (
-        rc["status"]      ==  status_code || # 200
-        rc["status"].to_i == status_code     # "200 OK"
+        rc["status"]      == status_code || # 200
+        rc["status"].to_i == status_code    # "200 OK"
       )
     end
 
