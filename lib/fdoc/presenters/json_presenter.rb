2679bdf442c8c2477c3d970c6f14af6d64b66675
Wei <jrweizhang@gmail.com>
2013-10-12 00:28:13 +0800

    Update Oj indention to 4.

--- lib/fdoc/presenters/json_presenter.rb
+++ lib/fdoc/presenters/json_presenter.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: utf-8
 require 'erb'
 require 'oj'
 class Fdoc::JsonPresenter
@@ -17,14 +17,14 @@ class Fdoc::JsonPresenter
       '<tt>%s</tt>' % json
     elsif json.kind_of?(Hash) ||
           json.kind_of?(Array)
-      '<pre><code>%s</code></pre>' % Oj.dump(json)
+      '<pre><code>%s</code></pre>' % Oj.dump(json, indent: 4)
     end
   end
 
   def to_markdown
     if json.kind_of?(Hash) ||
        json.kind_of?(Array)
-      Oj.dump(json)
+      Oj.dump(json, indent: 4)
     else
       json
     end
